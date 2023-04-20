32f23e6008f39b142b2378868de17ec6ee176a4c
fad16papa <fad16papa@gmail.com>
2023-04-21 00:59:06 +0800

    Commit changes in product controller

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -13,24 +14,23 @@ namespace API.Controllers
     public class ProductController : ControllerBase
     {
         private readonly StoreContext _context;
-
         public ProductController(StoreContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = _context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
 
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return _context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
