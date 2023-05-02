f4ce5189fe2dc95ae6989aa377ebd8140933a5c6
fad16papa <fad16papa@gmail.com>
2023-05-02 19:15:03 +0800

    Commit cahgnes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
+import { Typography } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -25,7 +26,7 @@ function addProduct(){
 
   return (
     <div>
-      <h1>Re-Store</h1>
+      <Typography variant="h1">React Store</Typography>
       <Catalog products={products} addProduct={addProduct}/>
       
     </div>
