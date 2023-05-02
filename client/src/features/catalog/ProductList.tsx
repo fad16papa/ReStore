2f804a8af052a578ccc5023ea559b240624a013d
fad16papa <fad16papa@gmail.com>
2023-05-02 21:13:24 +0800

    Commit changes

---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,23 @@
+import { List, ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    products: Product[];
+}
+
+export default function ProductList({products}: Props) {
+    return (
+        <List>
+        {products.map(product => (
+            <ListItem key={product.id}>
+                <ListItemAvatar>
+                    <Avatar src={product.pictureUrl}/>
+                </ListItemAvatar>
+                <ListItemText>
+                    {product.name} - {product.price}
+                </ListItemText>
+            </ListItem>
+        ))}
+    </List>
+    )
+}
