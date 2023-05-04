58fc1243b2714ca38cfcc93c49b01b69f6b05d23
fad16papa <fad16papa@gmail.com>
2023-05-05 03:28:01 +0800

    Commit changes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,15 @@
-import { Fragment, useEffect, useState } from "react";
-import { Product } from "../models/product";
+import { Fragment } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline } from "@mui/material";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5184/api/Product")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  function addProduct() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: "product" + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: "some brand",
-        description: "some description",
-        pictureUrl: "http://picsum.photos/200",
-      },
-    ]);
-  }
-
   return (
     <Fragment>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
     </Fragment>
   );
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Product } from "../../app/models/product";
-import { Button } from "@mui/material";
 import ProductList from "./ProductList";
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default function Catalog({products, addProduct}: Props) {
-    return (
-        <Fragment>
-            <ProductList products={products}/>
-            <Button variant="contained" onClick={addProduct}>Add Product</Button>
-        </Fragment>
+  useEffect(() => {
+    fetch("http://localhost:5184/api/Product")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    )
+  return (
+    <Fragment>
+      <ProductList products={products} />
+    </Fragment>
+  );
 }
